270f48220fcbdbd650dab68781b4de0f72b67c92
KenzoVdW-immalle <kenzo.vandewinckel@immalle.be>
2017-05-14 20:22:21 +0200

    form toegevoegd

--- resources/views/taaktoevoegen.blade.php
+++ resources/views/taaktoevoegen.blade.php
@@ -1,11 +1,29 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
-
 <title></title>
-
 </head>
 <body>
+
 <h1>een taak toevoegen</h1>
+<form>
+    <hr>
+    <label for="title">Title</label>
+    <input type="text" id="title" name"title">
+<br>
+    <form>
+    <label for="beschrijving">beschrijving</label>
+    <input type="text" id="beschrijving" name"beschrijving">
+
+    <br>
+    <form>
+    <label for="date">Datum wanneer de taak af moet zijn</label>
+    <input type="date" id="doel" name"doel">
+    <br>
+    <button type= "submit">Toevoegen</button>
+
+
+
 </body>
-</html>
+</html> 
+
